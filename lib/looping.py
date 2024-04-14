cfda2c35f56528110c7c0efe9991ab8bede47368
Jonathan MacDuff <jonathan.macduff@outlook.com>
2024-04-14 15:06:22 -0700

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+        print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_list = [i * i for i in int_list]
+    return int_list
+        
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if (i / 3 == i // 3) and (i / 5 == i // 5):
+            print("FizzBuzz")
+        elif (i / 3 == i // 3):
+            print("Fizz")
+        elif (i / 5 == i // 5):
+            print("Buzz")
+        else:
+            print(i)
